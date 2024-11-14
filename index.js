823d65c46e6ee4a553a4ec56d27cc6d16ddf3e93
sainath2216 <sainathmusham043@gmail.com>
2024-11-14 20:21:08 +0530

    api error

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ connection();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Welcome to User database");
-  })
+
 
 // routes
 app.use("/api/register", userRoutes);
